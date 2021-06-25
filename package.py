495b57482e67decfe1afc5db0c04543305a64503
Mehdi Bendriss <mehdi.bendriss@scout24.com>
2021-06-25 15:23:19 +0200

    Added a package script for zipping the lambda

---
+++ package.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+from shutil import copytree, make_archive, rmtree
+
+SRC_DIR = 'src/python'
+DEST_DIR = 'target/emr-autoscaling'
+
+if Path(DEST_DIR).exists() and Path(DEST_DIR).is_dir():
+    rmtree(DEST_DIR)
+
+copytree(SRC_DIR, DEST_DIR)
+
+make_archive(DEST_DIR, 'zip', DEST_DIR)
